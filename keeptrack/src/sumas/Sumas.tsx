6ca0b5744a9a5f66858dad7f5d04a8bfe6c9518c
Valgarite <93498358+Valgarite@users.noreply.github.com>
2023-04-19 21:15:10 -0400

    funcionando1

--- keeptrack/src/sumas/Sumas.tsx
+++ keeptrack/src/sumas/Sumas.tsx
@@ -8,11 +8,15 @@ const generarNumerosAleatorios = (min: number, max: number) => {
     const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
     resultado.push(randomNum)
   }
+
   return resultado
 }
 
+
 function SumasPage() {
   const [confirmarVenta, setConfirmarVenta] = useState(false)
+  const [numEjercicio, setNumEjercicio] = useState(0)
+  const abrirEjercicio= (i:number) => setNumEjercicio(i)
   const [numerosAleatorios] = useState(generarNumerosAleatorios(1, 9))
   const [numerosAleatorios2] = useState(generarNumerosAleatorios(1, 9))
   let resultados: number[] = []
@@ -23,14 +27,38 @@ function SumasPage() {
     }
     return resultados
   }
+
   const [resultadosParaMostrar] = useState(resultadosEjercicio)
-  const handleConfirm = () => setConfirmarVenta(true)
+  const handleConfirm = (i: number) => {
+    abrirEjercicio(i)
+    setTextoResultado('')
+    setConfirmarVenta(true)
+  }
   const handleConfirmClose = () => setConfirmarVenta(false);
+  const [textoResultado, setTextoResultado] = useState('')
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const userAnswer = parseInt(event.target.value);
+    setInputValue(event.target.value)
+  }
+
+  const validarEjercicio = (num1: number, num2: number, respuestaUsuario: string) => {
+    const respuestaInt = parseInt(respuestaUsuario)
+    const respuestaReal = num1 + num2
+    if (respuestaReal == respuestaInt) {
+      setTextoResultado('Correcto')
+    } else {
+      setTextoResultado('¡Vuelve a intentarlo!')
+    }
+  }
+
   return (
     <>
-      <p>{numerosAleatorios.map(numero => <span key={numero}>     {numero}</span>)}</p>
-      <p>{numerosAleatorios2.map(numero => <span key={numero}>     {numero}</span>)}</p>
-      <button onClick={handleConfirm}>xd</button>
+      <p>{numerosAleatorios.map((numero, i) => <span key={i}>{numero}     </span>)}</p>
+      <p>{numerosAleatorios2.map((numero, i) => <span key={i}>{numero}     </span>)}</p>
+      <p>{resultadosParaMostrar.map((i, index) => <span key={index}><button onClick={() => handleConfirm(index)}>{i}</button></span>)}</p>
+
       <Modal
         className="Modal-SeleccionarCliente"
         show={confirmarVenta}
@@ -42,7 +70,11 @@ function SumasPage() {
         <Modal.Body>
           <Card>
             <CardHeader>
-              {resultadosParaMostrar.map(numero => <span key={numero}>{numero}   </span>)}
+              <p>{numerosAleatorios[numEjercicio]}</p>
+              <p>{numerosAleatorios2[numEjercicio]}</p>
+              <p>{resultadosParaMostrar[numEjercicio]}</p>
+              <input type='text' value={inputValue} onChange={handleInputChange}></input>
+              <button onClick={() => validarEjercicio(numerosAleatorios[numEjercicio], numerosAleatorios2[numEjercicio], inputValue)}>{textoResultado}</button>
             </CardHeader>
           </Card>
         </Modal.Body>
